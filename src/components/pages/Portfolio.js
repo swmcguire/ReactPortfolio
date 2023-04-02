642f4266dc83739072d45f784bf2e13f4c30cbd3
swmcguire <swmcguire1@comcast.net>
2023-04-02 17:27:14 -0500

    added resume/ download feature - cleaned up images in the folder - Need to finalize the portfolio page and CSS

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,12 +1,71 @@
 import React from 'react';
+import EmpTracker from './../../Assets/images/Employee-Tracker.png';
+import JateApp from './../../Assets/images/JATE-App.png';
+import OrigPortfolio from './../../Assets/images/Original-Portfolio.png';
+import PassGen from './../../Assets/images/PasswordGenerator..png';
+import JScriptQuiz from './../../Assets/images/Quiz-Game.png';
+import FoodApi from './../../Assets/images/FoodAPIApp.png';
+
+const projects = [
+    {
+        title: 'Employee Tracker',
+        image: EmpTracker,
+        deployLink: 'No Link Available',
+        repoLink: 'https://github.com/swmcguire/SQL_Employee_Tracker',
+        altTag: 'Employee Tracker App',
+    },
+    {
+        title: 'JATE App',
+        image: JateApp,
+        deployLink: 'https://calm-sands-43698.herokuapp.com/',
+        repoLink: 'https://github.com/swmcguire/PWA-TextEditor',
+        altTag: 'JATE Persistence Text App',
+    },
+    {
+        title: 'Original html Portfolio',
+        image: OrigPortfolio,
+        deployLink: ' https://swmcguire.github.io/Challenge-2/',
+        repoLink: 'https://github.com/swmcguire/Challenge-2',
+        altTag: 'Original Portfolio',
+    },
+    {
+        title: 'Password Generator',
+        image: PassGen,
+        deployLink: 'https://swmcguire.github.io/Challenge-3/',
+        repoLink: 'https://github.com/swmcguire/Password-Generator',
+        altTag: 'Password Generator',
+    },
+    {
+        title: 'JavaScript Quiz Game',
+        image: JScriptQuiz,
+        deployLink: 'https://swmcguire.github.io/Code-Quiz-Game/',
+        repoLink: 'https://github.com/swmcguire/Code-Quiz-Game',
+        altTag: 'JavaScript Quiz Game',
+    },
+    {
+        title: 'Recipe Randomizer API App',
+        image: FoodApi,
+        deployLink: 'https://wir3s.github.io/eat-drink-be-merry/',
+        repoLink: 'https://github.com/swmcguire/eat-drink-be-merry',
+        altTag: 'Recipe API Generator',
+    },
+]
 
 function Portfolio() {
     return (
+        <article>
+        <h1> </h1>
         <div>
-            <h1>Portfolio Page</h1>
-            <p>This is my Portfolio</p>
+            <img
+                src={require('./../../Assets/images/Employee-Tracker.png')}
+                alt='Employee Tracker App'
+
+            />
         </div>
-    );
+    </article>
+    )
 }
+
+
 
 export default Portfolio;
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import SeanResume from './../../Assets/SeanMcGuireResume.docx'
 
 function Resume() {
     return (
         <div>
-            <h1>Resume Page</h1>
-            <p>This is my resume</p>
+            <h1>Resume</h1>
+            <p>Below is my resume with my past employment experience.  Please take a look and if you have any questions feel free to reach out on the Contacts tab.</p>
+            <a href={SeanResume} Download='SeanMcGuireResume.docx'>Download Sean's Resume</a>
+            <br></br>
+            <h2>Sean's Proficiencies</h2>
+            <ul>
+                <li>SQL</li>
+                <li>Node.js</li>
+                <li>React</li>
+                <li>REST APIs</li>
+                <li>HTML</li>
+                <li>Command Line Prompts</li>
+            </ul>
         </div>
     );
 }
